d7c093aadbb57824f7afb72243f8fb554fc538d2
dmkskn <dmksknn@gmail.com>
2019-01-12 15:10:23 +0300

    Initial commit

---
+++ polka.py
@@ -0,0 +1,29 @@
+"""модуль для доступа к API проекта «Полка»."""
+
+
+def books():
+    pass
+
+
+def search():
+    pass
+
+
+def lists():
+    pass
+
+
+def experts():
+    pass
+
+
+class Book:
+    pass
+
+
+class Expert:
+    pass
+
+
+class Compilation:
+    pass
